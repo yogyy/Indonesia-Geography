234ffad9ce7e2b9a4b130ed5063d4ea3879826c1
yogyy <yogimr261@gmail.com>
2023-06-16 18:10:37 +0700

    add hamburger and update navbar

---
+++ app/comp/hamburger.tsx
@@ -0,0 +1,56 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { cn } from '@/lib/utils';
+import { AlignJustify, AlignLeft } from 'lucide-react';
+import React from 'react';
+import { SideNavLink } from './nav-link';
+
+export function SheetDemo() {
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const alignedComponent = React.useMemo(() => {
+    return isHovered ? <AlignLeft /> : <AlignJustify />;
+  }, [isHovered]);
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="link" className="text-white">
+          <AlignJustify />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        position="left"
+        size="xl"
+        className="flex flex-col items-start text-white border-none bg-foreground sm:hidden"
+      >
+        <SheetHeader>
+          <SheetTitle className="flex w-full text-base text-white">
+            <AlignLeft className="mx-2.5" /> <span className="text-green-600">Nusantara</span>
+          </SheetTitle>
+          <hr />
+        </SheetHeader>
+        <ul className="w-full">
+          <SideNavLink />
+        </ul>
+      </SheetContent>
+    </Sheet>
+  );
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -3,6 +3,7 @@ import { ComboboxDemo } from '@/components/client/combobox';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import { NavLink } from './comp/nav-link';
+import { SheetDemo } from './comp/hamburger';
 
 // export const nameAtom = atom({ provinsi: 'Province', kabupaten: 'Regency', kecamatan: 'District' });
 
@@ -14,10 +15,15 @@ interface Provinsi {
 export default async function Navbar() {
   return (
     <header className="">
-      <nav className="">
-        <ul className="flex items-center h-full bg-emerald-700 py-2">
-          <NavLink />
-          <li className="relative"></li>
+      <nav className="flex items-center justify-center sm:px-10">
+        <div className="block sm:hidden">
+          <SheetDemo />
+        </div>
+        <p className="text-base font-semibold text-green-600">Nusantara</p>
+        <ul className="flex items-center justify-center w-full h-full py-2 supports-backdrop-blur:bg-background/60 bg-background/95 backdrop-blur">
+          <div className="hidden sm:flex">
+            <NavLink />
+          </div>
         </ul>
       </nav>
     </header>
