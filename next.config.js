3b30dc1b99d645bb9bee1bf06a6b195360b908d8
yogyy <yogimr261@gmail.com>
2023-06-28 16:41:19 +0700

    using withBundleAnalyzer

--- next.config.js
+++ next.config.js
@@ -9,5 +9,9 @@ const nextConfig = {
     },
   },
 };
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({});
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
